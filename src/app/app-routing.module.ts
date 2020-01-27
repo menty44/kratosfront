339f1e9764e28c3ee0f8a031f55a6a152fcb2c21
Fredrick Oluoch <menty44@gmail.com>
2020-01-27 08:39:40 +0300

    wip UI/UX

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from "./login/login.component";
+import { ContactComponent } from "./contact/contact.component";
+import { RegisterComponent } from "./register/register.component";
+import { ReportsComponent } from "./reports/reports.component";
+import { PricingComponent } from "./pricing/pricing.component";
+import { DisclaimerComponent } from "./disclaimer/disclaimer.component";
+import { LoadaccountComponent } from "./loadaccount/loadaccount.component";
+import { FinanceComponent } from "./finance/finance.component";
+import {ResultsComponent} from "./results/results.component";
+import {ResetpasswordComponent} from "./resetpassword/resetpassword.component";
+import {HomeComponent} from "./home/home.component";
+import {DownloadComponent} from "./download/download.component";
+import {PrivacyComponent} from "./privacy/privacy.component";
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'report', component: ReportsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'pricing', component: PricingComponent },
+  { path: 'disclaimer', component: DisclaimerComponent },
+  { path: 'loadaccount', component: LoadaccountComponent },
+  { path: 'finance', component: FinanceComponent },
+  { path: 'results', component: ResultsComponent },
+  { path: 'reset', component: ResetpasswordComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'download', component: DownloadComponent },
+  { path: 'privacy', component: PrivacyComponent },
+  { path: '', pathMatch:'full', redirectTo: '/home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,55 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ResetpasswordComponent } from './resetpassword/resetpassword.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashmenuComponent } from './dashmenu/dashmenu.component';
+import { LoadaccountComponent } from './loadaccount/loadaccount.component';
+import { ReportsComponent } from './reports/reports.component';
+import { ResultsComponent } from './results/results.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { DisclaimerComponent } from './disclaimer/disclaimer.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavigationserviceService } from 'services/navigationservice.service';
+import { FinanceComponent } from './finance/finance.component';
+import { HomeComponent } from './home/home.component';
+import { DownloadComponent } from './download/download.component';
+import { TeamComponent } from './team/team.component';
+import { PrivacyComponent } from './privacy/privacy.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    RegisterComponent,
+    LoginComponent,
+    ResetpasswordComponent,
+    DashboardComponent,
+    DashmenuComponent,
+    LoadaccountComponent,
+    ReportsComponent,
+    ResultsComponent,
+    PricingComponent,
+    DisclaimerComponent,
+    ContactComponent,
+    FinanceComponent,
+    HomeComponent,
+    DownloadComponent,
+    TeamComponent,
+    PrivacyComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    NavigationserviceService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
